5d52ec531860611b18c732dc3513e50ae9e6935e
poornima63 <poornima63061@gmail.com>
2025-02-07 15:06:46 +0530

    Add files via upload

---
+++ My Portfolio/assets/script.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    
+    const icon =document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open")
+    menu.classList.toggle("open")
+
+}
