13f5235c0717c354c300a106226ad99c834f8a7c
poornima63 <poornima63061@gmail.com>
2025-02-07 15:07:39 +0530

    Delete My Portfolio/assets directory

--- My Portfolio/assets/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(){
-    const menu=document.querySelector(".menu-links");
-    
-    const icon =document.querySelector(".hamburger-icon");
-
-    menu.classList.toggle("open")
-    menu.classList.toggle("open")
-
-}
