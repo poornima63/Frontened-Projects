b7006dd5ac6bc5e859e2f67437c3bd094afe6d74
poornima63 <poornima63061@gmail.com>
2025-02-11 12:57:48 +0530

    Add files via upload

---
+++ ResponsiveWebsite/myweb/script.js
@@ -0,0 +1,40 @@
+var crsr=document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+
+document.addEventListener("mousemove", function(dets){
+crsr.style.left=dets.x+"px"
+crsr.style.top=dets.y+"px"
+blur.style.left=dets.x -200+"px"
+blur.style.top=dets.y -200 +"px"
+    
+})
+
+
+
+
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    duration:0.5,
+    height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:3
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+         scroller:"body",
+         markers:true,
+         start:"top -50%",
+         end:"top -80%",
+         scrub:2
+    }
+})
